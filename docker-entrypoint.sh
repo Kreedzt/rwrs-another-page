dc07c0a73bea951a9bb50f650259a69a2f0c3e50
Kreedzt <zhaozisong1@live.com>
2025-03-27 20:30:59 +0800

    :wrench: update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,10 @@
 
 # 如果设置了 HEADER_SCRIPTS 环境变量，则注入脚本
 if [ ! -z "$HEADER_SCRIPTS" ]; then
+    # 使用临时文件来存储脚本内容
+    echo "$HEADER_SCRIPTS" > /tmp/header-scripts.txt
     # 使用 sed 在 index.html 的 </head> 标签前注入脚本
-    sed -i "s|</head>|${HEADER_SCRIPTS}</head>|" /usr/share/nginx/html/index.html
+    sed -i "/<\/head>/i $(cat /tmp/header-scripts.txt)" /usr/share/nginx/html/index.html
 fi
 
 # 启动 Nginx
