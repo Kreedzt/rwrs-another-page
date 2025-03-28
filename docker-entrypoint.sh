77c86bfe26c66f2628889a7a5c91c679f84b50f3
Kreedzt <zhaozisong1@live.com>
2025-03-28 09:56:14 +0800

    :wrench: use awk replace sed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@
 if [ ! -z "$HEADER_SCRIPTS" ]; then
     # 使用临时文件来存储脚本内容
     echo "$HEADER_SCRIPTS" > /tmp/header-scripts.txt
-    # 使用 sed 在 index.html 的 </head> 标签前注入脚本
-    sed -i -e '/<\/head>/i\' -e "$(cat /tmp/header-scripts.txt)" /usr/share/nginx/html/index.html
+    # 使用 awk 在 index.html 的 </head> 标签前注入脚本
+    awk '/<\/head>/ { system("cat /tmp/header-scripts.txt") } { print }' /usr/share/nginx/html/index.html > /tmp/index.html.tmp && mv /tmp/index.html.tmp /usr/share/nginx/html/index.html
 fi
 
 # 启动 Nginx
