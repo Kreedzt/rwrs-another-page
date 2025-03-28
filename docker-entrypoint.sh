023a2d7aa388970adfc5d9adff4ac5432c2b4926
Kreedzt <zhaozisong1@live.com>
2025-03-28 09:41:46 +0800

    :bug: fixed sed cmd

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -z "$HEADER_SCRIPTS" ]; then
     # 使用临时文件来存储脚本内容
     echo "$HEADER_SCRIPTS" > /tmp/header-scripts.txt
     # 使用 sed 在 index.html 的 </head> 标签前注入脚本
-    sed -i "/<\/head>/i $(cat /tmp/header-scripts.txt)" /usr/share/nginx/html/index.html
+    sed -i -e '/<\/head>/i\' -e "$(cat /tmp/header-scripts.txt)" /usr/share/nginx/html/index.html
 fi
 
 # 启动 Nginx
